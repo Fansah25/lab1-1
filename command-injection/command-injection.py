3d14a5fd8527848c2968ed8b6c5077fe440ff871
Faith Ansah <fansah@drew.edu>
2024-01-30 22:28:41 -0500

    commit to branch

--- command-injection/command-injection.py
+++ command-injection/command-injection.py
@@ -1,6 +1,17 @@
+#developer : Faith Ansah
 import os
 
 filename = input("Please provide a file name to search and display:\n")
 
-command = "cat " + filename
-os.system(command)
+if os.path.exists(filename):
+    print("File exists") 
+
+    with open(filename, "r") as f:
+        print(f.read())
+        f.close()
+        
+else:
+    print("File does not exist")
+
+#command = "cat " + filename
+#os.system(command)
